48db5c334d3df0ad0bfda25f689df549af31d15e
LeeWendao <LeeWendao@outlook.com>
2022-07-21 10:19:21 +0800

    Fix Bug:API '/getLatest' return null list
    - 优化了API的周期更新逻辑
    - 在getLatest的原有逻辑中，缺少错误处理流程。目前已补上，并加上了一个对目前发现的非常规邮件存储格式的丑陋解决方案

--- API/flask.py
+++ API/flask.py
@@ -1,5 +1,6 @@
-from flask import Flask,jsonify
-from threading import Timer
+from flask import Flask, jsonify
+import threading
+import time
 import zmail
 
 # @variable INFOQUEUE 常用的信息池
@@ -9,7 +10,12 @@ import zmail
 #   "topicNo":"xxx"
 #   "author":"xxx"
 # }]
-INFOQUEUE = []
+global info_queue
+info_queue = []
+
+user = ""
+password = ""
+theHost = ""
 
 class KEY:
     ID = "id"
@@ -17,79 +23,90 @@ class KEY:
     TOPICNO = "topicNo"
     AUTHOR = "author"
 
-def fuc_getINFOQUEUE():
-    global INFOQUEUE
-    INFOQUEUE.clear()
-    mailServer = zmail.server(user, password, smtp_host=theHost, smtp_port=25, pop_host=theHost,
-                pop_port=110, pop_ssl=False, pop_tls=False, smtp_ssl=False, smtp_tls=False)
-    theMailSum = mailServer.stat()[0]
-    mails = mailServer.get_mails(start_index=theMailSum-10,end_index=theMailSum)
-    for ele in mails:
-        ob = fuc_getObject()
-        ob[KEY.ID] = ele["id"]
-        temp = ele["subject"].split("/]")[1]
-        ob[KEY.TOPICNO] = ele["subject"].split("/]")[0].split("[")[1]
-        ob[KEY.TITLE] = temp.split("来自:")[0]
-        ob[KEY.AUTHOR] = temp.split("来自:")[1]
-        INFOQUEUE.append(ob)
-    INFOQUEUE.reverse()
+
+# @todo:添加异常字符串的解决方案（也许可以用正则表达式？）
+def fuc_updateLatest():
+    global info_queue
+    info_queue.clear()
+
+    try:
+        mailServer = zmail.server(user, password, smtp_host=theHost, smtp_port=25, pop_host=theHost,
+                                  pop_port=110, pop_ssl=False, pop_tls=False, smtp_ssl=False, smtp_tls=False)
+        print("更新Latest池 in " + time.asctime(time.localtime(time.time())))
+
+        theMailSum = mailServer.stat()[0]
+        mails = mailServer.get_mails(
+            start_index=theMailSum-10, end_index=theMailSum
+        )
+
+        for ele in mails:
+            ob = fuc_getObject()
+            ob[KEY.ID] = ele["id"]
+            subject = ele["subject"].split("/]")
+            # 遇到邮件非常规解析，出现如“[271194173/?start=0#4754211748]”等字样的临时丑陋解决方案
+            if len(subject) != 2:
+                first_crpt = subject[0].split("/")[0]
+                ob[KEY.TOPICNO] = first_crpt.split("[")[1]
+                second_crpt = subject[0].split("]")[1]
+                ob[KEY.TITLE] = second_crpt.split("来自:")[0]
+                ob[KEY.AUTHOR] = second_crpt.split("来自:")[1]
+                continue
+            temp = ele["subject"].split("/]")[1]
+            ob[KEY.TOPICNO] = ele["subject"].split("/]")[0].split("[")[1]
+            ob[KEY.TITLE] = temp.split("来自:")[0]
+            ob[KEY.AUTHOR] = temp.split("来自:")[1]
+            info_queue.append(ob)
+        info_queue.reverse()
+        print("正常完成更新")
+        
+    except Exception as e:
+        print("Error occurs in " + time.asctime(time.localtime(time.time())))
+        print(e)
+        info_queue.reverse()
+        print("此时，queue为:")
+        print(info_queue)
+    finally:
+        threading.Timer(120.0, fuc_updateLatest).start()
 
 
 def fuc_getObject():
     return {
-        "id":0,
-        "title":"null",
-        "topicNo":"null",
-        "author":"null"
+        "id": 0,
+        "title": "null",
+        "topicNo": "null",
+        "author": "null"
     }
-
-# def fuc_freshInstance():
-#     global mailServer
-#     if mailServer is None:
-#         mailServer = zmail.server(user, password, smtp_host=theHost, smtp_port=25, pop_host=theHost,
-#                    pop_port=110, pop_ssl=False, pop_tls=False, smtp_ssl=False, smtp_tls=False)
-#     else:
-#         del mailServer
-#         mailServer = zmail.server(user, password, smtp_host=theHost, smtp_port=25, pop_host=theHost,
-#                    pop_port=110, pop_ssl=False, pop_tls=False, smtp_ssl=False, smtp_tls=False) 
-
-def state_timeCycle():
-    fuc_getINFOQUEUE()
-    Timer(60*1,state_timeCycle).start()
-
-user = ""
-password = ""
-theHost = ""
 
 app = Flask(__name__)
 
-state_timeCycle()
+fuc_updateLatest()
 
-@app.route('/api/getLatest',methods=['GET'])
+@app.route('/api/getLatest', methods=['GET'])
 def getLatest():
-    global INFOQUEUE
-    return jsonify(str(INFOQUEUE))
+    global info_queue
+    return jsonify(str(info_queue))
 
-@app.route('/api/getmailInfo/<mailkey>',methods=['GET'])
+
+@app.route('/api/getmailInfo/<mailkey>', methods=['GET'])
 def getmailByKey(mailkey):
     mailServer = zmail.server(user, password, smtp_host=theHost, smtp_port=25, pop_host=theHost,
-                   pop_port=110, pop_ssl=False, pop_tls=False, smtp_ssl=False, smtp_tls=False)
+                              pop_port=110, pop_ssl=False, pop_tls=False, smtp_ssl=False, smtp_tls=False)
     content = mailServer.get_mail(mailkey)
-    return jsonify(str(content['content_html']).replace('\\n',''))
+    return jsonify(str(content['content_html']).replace('\\n', ''))
 
-@app.route('/api/search/<searchkey>',methods=['GET'])
+
+@app.route('/api/search/<searchkey>', methods=['GET'])
 def searchKey(searchkey):
     mailServer = zmail.server(user, password, smtp_host=theHost, smtp_port=25, pop_host=theHost,
-                   pop_port=110, pop_ssl=False, pop_tls=False, smtp_ssl=False, smtp_tls=False)
+                              pop_port=110, pop_ssl=False, pop_tls=False, smtp_ssl=False, smtp_tls=False)
     thelist = mailServer.get_mails(subject=searchkey)
     resultsList = []
     for ele in thelist:
         ob = {
-            "id":"null",
-            "title":"null"
+            "id": "null",
+            "title": "null"
         }
         ob[KEY.ID] = ele['id']
-        ob[KEY.TITLE] = ele['subject']
         resultsList.append(ob)
     resultsList.reverse()
     return jsonify(str(resultsList))
